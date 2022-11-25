c8567aa1f09ca6f23f3f7c1b2fb8009922342cd8
Lingcao Huang <huanglingcao@gmail.com>
2022-11-25 16:07:21 -0700

    image segmentation; originally for ArcticDEM

---
+++ grey_image_segment.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# Filename: grey_image_segment.py 
+"""
+introduction: segment a grey image (8 bit)
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 01 March, 2021
+"""
+
+import os,sys
+from optparse import OptionParser
+
+import raster_tools
+import utility
+
+from image_segment import quickshift_segmentaion
+from image_segment import mean_shift_segmentation
+from image_segment import felzenszwalb_segmentation
+
+from skimage import measure
+
+import multiprocessing
+from multiprocessing import Pool
+import numpy as np
+
+import cv2
+
+def get_stastics_from_array(in_array, nodata,range=None):
+    data_1d = in_array.flatten()
+    data_1d = data_1d[ data_1d != nodata]
+    data_1d = data_1d[~np.isnan(data_1d)]  # remove nan value
+
+    # current, only calculate the mean
+    values = []
+    values.append(np.mean(data_1d).astype(np.float64)) # if data_1d is empty, then it returns None.     # save to float64 for json
+    values.append(np.std(data_1d).astype(np.float64))  # std
+    values.append(data_1d.size)
+    return values
+
+def segment_a_patch(idx, patch, patch_count,img_path, org_raster,b_save_patch_label):
+
+    print('tile: %d / %d' % (idx + 1, patch_count))
+    image_name_no_ext = utility.get_name_no_ext(img_path)
+    patch_dir = image_name_no_ext + '_patch%d'%idx
+    patch_label_path = os.path.join(patch_dir, image_name_no_ext+'_patch%d_label.tif'%idx)
+    if b_save_patch_label is True:
+        if os.path.isdir(patch_dir) is False:
+            utility.mkdir(patch_dir)
+        if os.path.isfile(patch_label_path):
+            print('%s exists, skip'%patch_label_path)
+            return patch, patch_label_path, None, None
+
+    # read imag
+    one_band_img, nodata = raster_tools.read_raster_one_band_np(img_path, boundary=patch)
+
+    # # apply median filter to image (remove some noise)
+    one_band_img = cv2.medianBlur(one_band_img, 3)  # with kernal=3, cannot accept int32
+
+    # segmentation algorithm (the output of these algorithms is not alway good, need to chose the parameters carafully)
+    # out_labels = watershed_segmentation(one_band_img)
+    # out_labels = k_mean_cluster_segmentation(one_band_img)
+    out_labels = quickshift_segmentaion(one_band_img,ratio=0.3, kernel_size=5, max_dist=10,
+                           sigma=1, convert2lab=False)
+
+    # out_labels = felzenszwalb_segmentation(one_band_img,scale=3.0, sigma=0.95, min_size=5)
+
+    #
+    #
+    # out_labels = mean_shift_segmentation(one_band_img)
+
+    # print('min and max labels of out_labels', np.min(out_labels), np.max(out_labels))
+
+    if b_save_patch_label is True:
+        # save the label
+        raster_tools.save_numpy_array_to_rasterfile(out_labels, patch_label_path, img_path, boundary=patch) # it copy nodata, need to unset it later
+        return patch, patch_label_path, None, None
+
+    # calculate the attributes based on orginal data for original data
+    object_attributes = {}  # object id (label) and attributes (list)
+    if org_raster is not None:
+        org_img_b1, org_nodata = raster_tools.read_raster_one_band_np(org_raster, boundary=patch)
+
+        # get regions (the labels output by segmentation is not unique for superpixels)
+        # regions = measure.regionprops(out_labels, intensity_image=org_img_b1)     # regions is based on out_labels, so it has the same issue.
+        # print('region count from sk-image measure:',len(regions))
+
+        label_list = np.unique(out_labels)
+        # get statistics for each segmented object (label)
+        for label in label_list:
+            in_array = org_img_b1[ out_labels == label ]
+            object_attributes[label] = get_stastics_from_array(in_array, org_nodata)
+
+        return patch, out_labels, nodata, object_attributes
+
+    return patch, out_labels, nodata, None
+
+def segment_a_grey_image(img_path, save_dir,process_num, org_raster=None,b_save_patch_label=False):
+
+    out_pre = os.path.splitext(os.path.basename(img_path))[0]
+    label_path = os.path.join(save_dir, out_pre + '_label.tif')
+    if os.path.isfile(label_path):
+        print('%s exist, skip segmentation'%label_path)
+        return label_path
+
+    height, width, band_num, date_type = raster_tools.get_height_width_bandnum_dtype(img_path)
+    print('input image: height, width, band_num, date_type',height, width, band_num, date_type)
+
+    # if the original data is available, then calculate the attributes based on that
+    if org_raster is not None:
+        org_height, org_width, org_band_num, org_date_type = raster_tools.get_height_width_bandnum_dtype(org_raster)
+        if org_height != height or org_width != width:
+            raise ValueError('%s and %s do not have the same size'%(img_path,org_raster))
+
+    save_labes = np.zeros((height,width),dtype=np.int32)
+    # divide the image the many small patches, then calcuate one by one, solving memory issues.
+    image_patches = raster_tools.sliding_window(width,height, 1024, 1024,adj_overlay_x=0,adj_overlay_y=0)
+    patch_count = len(image_patches)
+
+    # for idx, patch in enumerate(image_patches):
+    #     out_patch,out_labels = segment_a_patch(idx, patch, patch_count,img_path)
+    #     # copy to the entire image
+    #     row_s = patch[1]
+    #     row_e = patch[1] + patch[3]
+    #     col_s = patch[0]
+    #     col_e = patch[0] + patch[2]
+    #     save_labes[row_s:row_e, col_s:col_e] = out_labels
+
+    theadPool = Pool(process_num)
+    parameters_list = [ (idx, patch, patch_count,img_path, org_raster,b_save_patch_label) for idx, patch in enumerate(image_patches)]
+    results = theadPool.starmap(segment_a_patch, parameters_list)
+
+    patch_label_path_list = []
+    patch_label_id_range = []
+    object_attributes = {}  # object id (label) and attributes (list)
+    for res in results:
+        patch, out_labels, nodata, attributes = res
+        if isinstance(out_labels, str) and os.path.isfile(out_labels):  #if it's a label file
+            patch_label_path_list.append(out_labels)
+        else:
+            # copy to the entire image
+            row_s = patch[1]
+            row_e = patch[1] + patch[3]
+            col_s = patch[0]
+            col_e = patch[0] + patch[2]
+            current_min = np.max(save_labes)
+            print('current_max',current_min)
+            patch_label_id_range.append(current_min)
+            save_labes[row_s:row_e, col_s:col_e] = out_labels + current_min + 1
+            if attributes is not None:
+                update_label_attr = {}
+                for key in attributes:
+                    update_label_attr[ key + current_min] = attributes[key]
+                # add to the attributes
+                object_attributes.update(update_label_attr)
+
+    theadPool.close()
+    # # apply median filter (remove some noise), #  we should not use median  filter, because it's labels, not images.
+    # label_blurs = cv2.medianBlur(np.float32(save_labes), 3)  # with kernal=3, cannot accept int32
+    # # print(label_blurs, label_blurs.dtype)
+    # save_labes = label_blurs.astype(np.int32)
+
+    # return a list of labels saved in current working folder.
+    if b_save_patch_label:
+        return patch_label_path_list
+
+    if os.path.isdir(save_dir) is False:
+        utility.mkdir(save_dir)
+
+    # save attributes (if not empty)
+    if object_attributes:
+        attribute_path = os.path.join(save_dir, out_pre + '_attributes.txt')
+        utility.save_dict_to_txt_json(attribute_path,object_attributes)
+
+    # save the label
+    raster_tools.save_numpy_array_to_rasterfile(save_labes, label_path, img_path) # do not set nodata
+    # save id ranges to txt
+    label_id_range_txt = os.path.splitext(label_path)[0] + '_IDrange.txt'
+    patch_label_id_range = [str(item) for item in patch_label_id_range]
+    utility.save_list_to_txt(label_id_range_txt,patch_label_id_range)
+
+
+    return label_path
+
+
+def test_segment_a_grey_image():
+    data_dir = os.path.expanduser('~/Data/Arctic/canada_arctic/DEM/WR_dem_diff')
+    img_path = os.path.join(data_dir,'WR_extent_grid_ids_DEM_diff_grid9274_8bit.tif')
+    # org_raster = os.path.join(data_dir,'WR_extent_grid_ids_DEM_diff_grid9274.tif')
+    save_dir = os.path.join(data_dir,'segment_parallel_9274')
+    process_num = 8
+    b_save_patch_label = True
+    segment_a_grey_image(img_path, save_dir, process_num, org_raster=None, b_save_patch_label=b_save_patch_label)
+
+
+def main(options, args):
+
+    img_path = args[0]
+    utility.is_file_exist(img_path)
+    save_dir = options.save_dir
+    process_num = options.process_num
+    org_elevation_diff = options.elevation_diff
+
+    segment_a_grey_image(img_path,save_dir,process_num,org_raster=org_elevation_diff)
+
+
+if __name__ == "__main__":
+    usage = "usage: %prog [options] image_path "
+    parser = OptionParser(usage=usage, version="1.0 2021-3-1")
+    parser.description = 'Introduction: segment a grey image using watershed algorithm   '
+
+    parser.add_option("", "--process_num",
+                      action="store", dest="process_num", type=int, default=1,
+                      help="number of processes to create the mosaic")
+
+    parser.add_option("-d", "--save_dir",
+                      action="store", dest="save_dir", default='./',
+                      help="the folder to save results")
+
+    parser.add_option("-c", "--elevation_diff",
+                      action="store", dest="elevation_diff",
+                      help="the original elevation difference")
+
+    (options, args) = parser.parse_args()
+
+    if len(sys.argv) < 2 or len(args) < 1:
+        parser.print_help()
+        sys.exit(2)
+
+    main(options, args)
---
+++ image_segment.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# Filename: watershed_segment 
+"""
+introduction: segment a grey image
+
+authors: Huang Lingcao
+email:huanglingcao@gmail.com
+add time: 21 February, 2021
+"""
+
+import os,sys
+from optparse import OptionParser
+
+import raster_tools
+import utility
+
+import numpy as np
+from skimage import segmentation
+
+# import pymeanshift as pms
+
+import cv2
+
+def to_unique_label_for_superpixels(seg_label_image):
+    # input is the label image output by segmentation algorithm,
+    # sometime, two separate superpixels has the same label, cause problem later.
+
+    # use findContours to make each superpixel only have the unique pixel.
+    # findContours: https://docs.opencv.org/3.4/d3/dc0/group__imgproc__shape.html#ga17ed9f5d79ae97bd4c7cf18403e1689a
+
+
+    # notes: not working, need more understanding and test
+
+    # mode equals to RETR_CCOMP or RETR_FLOODFILL, the input can also be a 32-bit integer image of labels
+    contours, hierarchy = cv2.findContours(seg_label_image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    print('contours count', len(contours))
+    new_labels = np.zeros_like(seg_label_image)
+    for idx, cont in enumerate(contours):
+        # print(idx)
+        cv2.drawContours(new_labels, contours, idx, idx, thickness=-1) # ,hierarchy=hierarchy, maxLevel=2
+        # cv2.drawContours(new_labels, cont, 0, idx, thickness=-1) # hierarchy=hierarchy, maxLevel=2
+
+    print('hierarchy shape',hierarchy.shape)
+
+    return new_labels
+
+
+def watershed_segmentation(img_2d_one_band):
+    # the result is so bad when we applied this one to dem difference (float32), maybe we need choose some markers.
+    out_labels = segmentation.watershed(img_2d_one_band)
+    return out_labels
+
+def k_mean_cluster_segmentation(img_2d_one_band):
+    # n_segments=100, hard to decide this parameter.
+    # result is bad when applied to the dem difference (float32), need to adjust the parameters.
+    img_2d_one_band = img_2d_one_band.astype(np.float64)
+    out_labels = segmentation.slic(img_2d_one_band)
+    out_labels = out_labels.astype(np.int32)
+    return out_labels
+
+
+def felzenszwalb_segmentation(img_2d_one_band,scale=3.0, sigma=0.95, min_size=5):
+    # https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.felzenszwalb
+    out_labels = segmentation.felzenszwalb(img_2d_one_band, scale=scale, sigma=sigma, min_size=min_size)
+
+    return out_labels
+
+
+def quickshift_segmentaion(img_2d_one_band, ratio=1.0, kernel_size=5, max_dist=10, return_tree=False,
+                           sigma=0, convert2lab=True, random_seed=42):
+    # Segments image using quickshift clustering in Color-(x,y) space.
+    # Produces an oversegmentation of the image using the quickshift mode-seeking algorithm.
+
+    # https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_segmentations.html?highlight=segmentation
+    # Quickshift has two main parameters: sigma controls the scale of the local density approximation,
+    # max_dist selects a level in the hierarchical segmentation that is produced.
+    # There is also a trade-off between distance in color-space and distance in image-space, given by ratio.
+
+    # ValueError: the input array must be have a shape == (.., ..,[ ..,] 3)), got (1882, 1895, 1), set convert2lab=False
+
+    img_2d_one_band = img_2d_one_band.astype(np.float64)
+    out_labels = segmentation.quickshift(img_2d_one_band, ratio=ratio, kernel_size=kernel_size, max_dist=max_dist, return_tree=return_tree,
+                           sigma=sigma, convert2lab=convert2lab, random_seed=random_seed)
+    out_labels = out_labels.astype(np.int32)
+    return out_labels
+
+def mean_shift_segmentation(img_2d_one_band):
+    # python version of mean shift: https://github.com/fjean/pymeanshift
+    # clone this repo, then ./setup.py install --user
+
+    # after several test, set the parameters for 8bit grey image drived from elevation difference.
+    segmented_image, labels_image, number_regions = pms.segment(img_2d_one_band, spatial_radius=5,
+                                                                  range_radius=11, min_density=20)
+    return labels_image
+
+def segment_changes_on_dem_diff(dem_diff_tif, save_dir):
+
+    out_pre = os.path.splitext(os.path.basename(dem_diff_tif))[0]
+
+    # read images
+    one_band_img, nodata = raster_tools.read_raster_one_band_np(dem_diff_tif)
+
+    # segmentation algorithm (the output of these algorithms is not alway good, need to chose the parameters carafully)
+    # out_labels = watershed_segmentation(one_band_img)
+    # out_labels = k_mean_cluster_segmentation(one_band_img)
+    # out_labels = quickshift_segmentaion(one_band_img)
+    out_labels = mean_shift_segmentation(one_band_img)
+
+
+    # segmentation by threshold (may have too many noise)
+    # mean = np.nanmean(one_band_img)
+    # print("mean value is: %.4f"%mean)
+    # one_band_img = one_band_img - mean
+    # out_labels = np.zeros_like(one_band_img,dtype=np.uint8)
+    # out_labels[ np.abs(one_band_img) > 2 ] = 1
+
+    # save the label
+    label_path = os.path.join(save_dir, out_pre + '_label.tif')
+    raster_tools.save_numpy_array_to_rasterfile(out_labels, label_path, dem_diff_tif, nodata=0)
+
+    # convert the label to shapefile
+    out_shp = os.path.join(save_dir, out_pre + '.shp')
+    command_string = 'gdal_polygonize.py -8 %s -b 1 -f "ESRI Shapefile" %s' % (label_path, out_shp)
+    res = os.system(command_string)
+    if res != 0:
+        sys.exit(1)
+
+
+def main(options, args):
+
+    img_path = args[0]
+    utility.is_file_exist(img_path)
+    save_dir = options.save_dir
+    segment_changes_on_dem_diff(img_path,save_dir)
+
+    pass
+
+if __name__ == "__main__":
+
+    usage = "usage: %prog [options] image_path "
+    parser = OptionParser(usage=usage, version="1.0 2021-2-21")
+    parser.description = 'Introduction: segment a grey image using watershed algorithm   '
+
+    parser.add_option("", "--process_num",
+                      action="store", dest="process_num", type=int, default=1,
+                      help="number of processes to create the mosaic")
+
+    parser.add_option("-d", "--save_dir",
+                      action="store", dest="save_dir",default='./',
+                      help="the folder to save results")
+
+    (options, args) = parser.parse_args()
+
+    if len(sys.argv) < 2 or len(args) < 1:
+        parser.print_help()
+        sys.exit(2)
+
+    main(options, args)
