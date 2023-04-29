ba85d733566a8552b618326e29df0804c93ab49f
Lingcao Huang <huanglingcao@gmail.com>
2023-04-29 10:57:53 -0700

    add comments

--- container_test/test_docker.sh
+++ container_test/test_docker.sh
@@ -2,14 +2,14 @@
 
 set -eE -o functrace
 
-hostdir=/home/lihu9680/Bhaltos2/lingcaoHuang
+hostdir=~/Bhaltos2/lingcaoHuang
 input=/data/flooding_area/test_docker_containers/s1_pre-processing_FD_input_docker.json
 docker_home=/root
 
-# download 
+# download Sentinel-1 data
 docker run --rm -v ${hostdir}:/data -v $HOME:${docker_home}  -it sentinel-1-pre-processing asf_download.py ${input}
 
-# preprocessing
+# preprocessing of Sentinel-1 data
 docker run --rm -v ${hostdir}:/data -v $HOME:${docker_home}  -it sentinel-1-pre-processing snap_GRD_process.py  ${input} 
 
 #flood detection
